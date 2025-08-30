72a81de722dacaf2f618945268ee3f77179be8bb
biphband <147620984+biphband@users.noreply.github.com>
2025-08-30 11:24:24 +0800

    Add files via upload

--- thenutcracker/pdffiles.js
+++ thenutcracker/pdffiles.js
@@ -1,197 +1,48 @@
 const pdfFiles = {
-    'WOTF': {
-      Strings: [
-        'WOTF - Violin 1.pdf',
-        'WOTF - Violin 2.pdf',
-        'WOTF - Violin 3.pdf',
-        'WOTF - Violoncello.pdf',
-        'WOTF - Contrabass.pdf',
-        'WOTF - Harp.pdf'
-      ],
-      Woodwind: [
-        'WOTF - Piccolo.pdf',
-        'WOTF - Flute 1.pdf',
-        'WOTF - Flute 2.pdf',
-        'WOTF - Flute 3.pdf',
-        'WOTF - Oboe.pdf',
-        'WOTF - Clarinet 1.pdf',
-        'WOTF - Clarinet 2.pdf',
-        'WOTF - Clarinet 3.pdf',
-        'WOTF - Alto Sax 1.pdf',
-        'WOTF - Alto Sax 2.pdf',
-        'WOTF - Alto Sax 3.pdf',
-        'WOTF - Tenor Saxophone.pdf',
-        'WOTF - Bass Clarinet.pdf',
-        'WOTF - Bassoon.pdf'
-      ],
-      Brass: [
-        'WOTF - Trumpet.pdf',
-        'WOTF - Horn in F.pdf',
-        'WOTF - Trombone.pdf',
-        'WOTF - Euphonium.pdf',
-        'WOTF - Tuba.pdf'
-      ],
-      Percussion: [
-        'WOTF - Glockenspiel.pdf',
-        'WOTF - Vibraphone.pdf',
-        'WOTF - Marimba.pdf',
-        'WOTF - Aux Perc 1.pdf',
-        'WOTF - Aux Perc 2.pdf'
-      ],
-      Other: [
-        'WOTF - Conductor.pdf'
-      ]
-    },
-    'DOTSPF': {
-      Strings: [
-        'DOTSPF - Violin 1.pdf',
-        'DOTSPF - Violin 2.pdf',
-        'DOTSPF - Violin 3.pdf',
-        'DOTSPF - Violoncello.pdf',
-        'DOTSPF - Contrabass.pdf',
-        'DOTSPF - Contrabass 2.pdf'
-      ],
-      Woodwind: [
-        'DOTSPF - Flute I.pdf',
-        'DOTSPF - Flute II.pdf',
-        'DOTSPF - Flute III.pdf',
-        'DOTSPF - Clarinet 1.pdf',
-        'DOTSPF - Clarinet 2.pdf',
-        'DOTSPF - Clarinet 3.pdf',
-        'DOTSPF - Alto Sax 1.pdf',
-        'DOTSPF - Alto Sax 2.pdf',
-        'DOTSPF - Tenor Saxophone.pdf',
-        'DOTSPF - Bass Clarinet in Bb.pdf',
-        'DOTSPF - Bassoon.pdf'
-      ],
-      Brass: [
-        'DOTSPF - Trumpet 1.pdf',
-        'DOTSPF - Trumpet 2.pdf',
-        'DOTSPF - Horn II in F.pdf',
-        'DOTSPF - Horn III in F.pdf',
-        'DOTSPF - Trombone.pdf',
-        'DOTSPF - Euphonium.pdf',
-        'DOTSPF - Tuba.pdf'
-      ],
-      Percussion: [
-        'DOTSPF - Glockenspiel.pdf',
-        'DOTSPF - Vibraphone.pdf',
-        'DOTSPF - Marimba.pdf',
-        'DOTSPF - Celesta.pdf'
-      ],
-      Other: [
-        'DOTSPF - Conductor.pdf'
-      ]
-    },
-    'Russian Dance': {
-      Strings: [
-        'Russian Dance - Violin 1.pdf',
-        'Russian Dance - Violin 2.pdf',
-        'Russian Dance - Violin 3.pdf',
-        'Russian Dance - Violoncello.pdf',
-        'Russian Dance - Contrabass.pdf'
-      ],
-      Woodwind: [
-        'Russian Dance - Flute 1.pdf',
-        'Russian Dance - Flute 2.pdf',
-        'Russian Dance - Clarinet 1.pdf',
-        'Russian Dance - Clarinet 2.pdf',
-        'Russian Dance - Alto Sax 1.pdf',
-        'Russian Dance - Alto Sax 2.pdf',
-        'Russian Dance - Tenor Saxophone.pdf',
-        'Russian Dance - Bassoon.pdf'
-      ],
-      Brass: [
-        'Russian Dance - Trombone.pdf',
-        'Russian Dance - Euphonium.pdf',
-        'Russian Dance - Tuba.pdf'
-      ],
-      Percussion: [
-        'Russian Dance - Tambourine.pdf',
-        'Russian Dance - Marimba.pdf',
-        'Russian Dance - Bass Drum.pdf'
-      ],
-      Other: [
-        'Russian Dance - Conductor.pdf',
-        'Russian Dance - Piano.pdf'
-      ]
-    },
-    'Overture': {
-      Strings: [
-        'Overture - Violin 1.pdf',
-        'Overture - Violin 2.pdf',
-        'Overture - Violin 3.pdf',
-        'Overture - Cello 1.pdf',
-        'Overture - Cello 2.pdf',
-        'Overture - Contrabass 1.pdf',
-        'Overture - Contrabass 2.pdf',
-        'Overture - Harp.pdf'
-      ],
-      Woodwind: [
-        'Overture - Piccolo.pdf',
-        'Overture - Flute 1.pdf',
-        'Overture - Flute 2.pdf',
-        'Overture - Flute 3.pdf',
-        'Overture - Clarinet 1.pdf',
-        'Overture - Clarinet 2.pdf',
-        'Overture - Clarinet 3.pdf',
-        'Overture - Alto Sax 1.pdf',
-        'Overture - Alto Sax 2.pdf',
-        'Overture - Alto Sax 3.pdf',
-        'Overture - Tenor Sax.pdf',
-        'Overture - Bass Clarinet.pdf',
-        'Overture - Bassoon.pdf'
-      ],
-      Brass: [
-        'Overture - Trumpet 1.pdf',
-        'Overture - Trumpet 2.pdf',
-        'Overture - Trumpet 3.pdf',
-        'Overture - F Horn 1.pdf',
-        'Overture - Horn in F 2.pdf',
-        'Overture - Trombone 1.pdf',
-        'Overture - Trombone 2.pdf',
-        'Overture - Euphonium.pdf',
-        'Overture - Tuba.pdf'
-      ],
-      Percussion: [
-        'Overture - Glockenspiel.pdf',
-        'Overture - Vibraphone.pdf',
-        'Overture - Marimba.pdf',
-        'Overture - Timpani.pdf',
-        'Overture - Triangle.pdf'
-      ],
-      Other: [
-        'Overture - Conductor.pdf'
-      ]
-    },
-    'Chinese Dance': {
-      Strings: [
-        'Chinese Dance - Violin 1.pdf',
-        'Chinese Dance - Violin 2.pdf',
-        'Chinese Dance - Violin 3.pdf',
-        'Chinese Dance - Violoncello.pdf',
-        'Chinese Dance - Contrabass.pdf'
-      ],
-      Woodwind: [
-        'Chinese Dance - Piccolo.pdf',
-        'Chinese Dance - Flute 1.pdf',
-        'Chinese Dance - Flute 2.pdf',
-        'Chinese Dance - Bb Clarinet 1.pdf',
-        'Chinese Dance - Bb Clarinet 2.pdf',
-        'Chinese Dance - Bb Clarinet 3.pdf',
-        'Chinese Dance - Tenor Saxophone.pdf',
-        'Chinese Dance - Bassoon.pdf'
-      ],
-      Brass: [
-        'Chinese Dance - Trombone.pdf',
-        'Chinese Dance - Euphonium.pdf',
-        'Chinese Dance - Tuba.pdf'
-      ],
-      Percussion: [
-        'Chinese Dance - Glockenspiel.pdf'
-      ],
-      Other: [
-        'Chinese Dance - Conductor.pdf'
-      ]
-    }
+    Strings: [
+      'Violin_1_Parts.pdf',
+      'Violin_2_Parts.pdf',
+      'Violin_3_Parts.pdf',
+      'Cello_1_Parts.pdf',
+      'Cello_2_Parts.pdf',
+      'Contrabass_Parts.pdf'
+    ],
+    Woodwind: [
+      'Piccolo_Parts.pdf',
+      'Flute_1_Parts.pdf',
+      'Flute_2_Parts.pdf',
+      'Flute_3_Parts.pdf',
+      'Oboe_Parts.pdf',
+      'Clarinet_Parts.pdf',
+      'Alto_Sax_1_Parts.pdf',
+      'Alto_Sax_2_Parts.pdf',
+      'Alto_Sax_3_Parts.pdf',
+      'Tenor_Saxophone_Parts.pdf',
+      'Bassoon_Parts.pdf'
+    ],
+    Brass: [
+      'Trumpet_Parts.pdf',
+      'Horn_1_Parts.pdf',
+      'Horn_2_Parts.pdf',
+      'Trombone_Parts.pdf',
+      'Euphonium_Parts.pdf',
+      'Tuba_Parts.pdf'
+    ],
+    Percussion: [
+      'Glockenspiel_Parts.pdf',
+      'Vibraphone_Parts.pdf',
+      'Marimba_Parts.pdf',
+      'Timpani_Parts.pdf',
+      'Triangle_Parts.pdf',
+      'Tambourine_Parts.pdf',
+      'Bass_Drum_Parts.pdf',
+      'Cymbal_Parts.pdf',
+      'Aux_Perc_1_Parts.pdf',
+      'Aux_Perc_2_Parts.pdf',
+      'Sleigh_Bells_Parts.pdf',
+      'Piano_Parts.pdf'
+    ],
+    Other: [
+      'Conductor_Parts.pdf'
+    ]
   };
