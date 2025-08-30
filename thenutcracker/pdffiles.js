95613af64637bcee7a564515d15e7c626425dcfd
biphband <147620984+biphband@users.noreply.github.com>
2025-08-30 11:51:27 +0800

    Add files via upload

--- thenutcracker/pdffiles.js
+++ thenutcracker/pdffiles.js
@@ -5,7 +5,8 @@ const pdfFiles = {
       'Violin_3_Parts.pdf',
       'Cello_1_Parts.pdf',
       'Cello_2_Parts.pdf',
-      'Contrabass_Parts.pdf'
+      'Contrabass_1_Parts.pdf',
+      'Contrabass_2_Parts.pdf'
     ],
     Woodwind: [
       'Piccolo_Parts.pdf',
@@ -13,18 +14,26 @@ const pdfFiles = {
       'Flute_2_Parts.pdf',
       'Flute_3_Parts.pdf',
       'Oboe_Parts.pdf',
-      'Clarinet_Parts.pdf',
-      'Alto_Sax_1_Parts.pdf',
-      'Alto_Sax_2_Parts.pdf',
-      'Alto_Sax_3_Parts.pdf',
+      'Clarinet_1_Parts.pdf',
+      'Clarinet_2_Parts.pdf',
+      'Clarinet_3_Parts.pdf',
+      'Alto_sax_1_Parts.pdf',
+      'Alto_sax_2_Parts.pdf',
+      'Alto_sax_3_Parts.pdf',
       'Tenor_Saxophone_Parts.pdf',
+      'Bass_clarinet_Parts.pdf',
       'Bassoon_Parts.pdf'
     ],
     Brass: [
       'Trumpet_Parts.pdf',
+      'Trumpet_1_Parts.pdf',
+      'Trumpet_2_Parts.pdf',
+      'Trumpet_3_Parts.pdf',
       'Horn_1_Parts.pdf',
       'Horn_2_Parts.pdf',
       'Trombone_Parts.pdf',
+      'Trombone_1_Parts.pdf',
+      'Trombone_2_Parts.pdf',
       'Euphonium_Parts.pdf',
       'Tuba_Parts.pdf'
     ],
@@ -35,11 +44,11 @@ const pdfFiles = {
       'Timpani_Parts.pdf',
       'Triangle_Parts.pdf',
       'Tambourine_Parts.pdf',
-      'Bass_Drum_Parts.pdf',
+      'Bass_drum_Parts.pdf',
       'Cymbal_Parts.pdf',
-      'Aux_Perc_1_Parts.pdf',
-      'Aux_Perc_2_Parts.pdf',
-      'Sleigh_Bells_Parts.pdf',
+      'Aux_perc_1_Parts.pdf',
+      'Aux_perc_2_Parts.pdf',
+      'Sleigh_bells_Parts.pdf',
       'Piano_Parts.pdf'
     ],
     Other: [
